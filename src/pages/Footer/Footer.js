3f69d1b8aed86ef3c0dd48c443dc90fece9105a9
Hossain Ahmad <hossain177922@gmail.com>
2022-11-10 21:57:44 +0600

    footer

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
             <div>
                 <span className="footer-title">Services</span>
                 <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
+                <a className="link link-hover">Photography</a>
                 <a className="link link-hover">Marketing</a>
                 <a className="link link-hover">Advertisement</a>
             </div>
