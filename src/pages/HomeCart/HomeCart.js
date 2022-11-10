6cfd89b57084de3c21b2a975d722df17017e0b40
Hossain Ahmad <hossain177922@gmail.com>
2022-11-10 21:38:05 +0600

    added pic

--- src/pages/HomeCart/HomeCart.js
+++ src/pages/HomeCart/HomeCart.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import img from '../../asset/img/deer.webp';
+
 
 const HomeCart = () => {
     return (
         <div className='mt-12 mb-12'>
             <div className="hero rounded  bg-base-200">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src={img} alt="" />
+                    <img className='w-[50%] rounded' src="https://images.unsplash.com/photo-1551705459-5611b0d0b60e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="" />
                     <div>
                         <h1 className="text-5xl font-bold">Deer is a beautiful <br /> animal</h1>
                         <p className="py-6 font-medium">The two main groups of deer are the Cervinae, including the muntjac, the elk, the red deer, and the fallow deer</p>
